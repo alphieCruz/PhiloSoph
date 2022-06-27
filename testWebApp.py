2b9dbd892322c3c113ecc423920f570301e2fd1c
Alejandro Cruz <108286926+alphieCruz@users.noreply.github.com>
2022-06-27 11:18:53 -0500

    Update testWebApp.py

--- testWebApp.py
+++ testWebApp.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import requests
+import traceback
 
 st.write('hello web app here')
 
-st.write(requests.get('http://179.50.15.34:5000').text)
+try:
+  st.write(requests.get('http://179.50.15.34:5000').text)
+ except:
+  print(traceback.format_exc())
