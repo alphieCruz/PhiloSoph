8917b9dc0c989b64b5a8b11edc47f3ef72ec9af9
Alejandro Cruz <108286926+alphieCruz@users.noreply.github.com>
2022-06-27 11:22:49 -0500

    Update testWebApp.py

--- testWebApp.py
+++ testWebApp.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import requests
-import traceback
+import sys
 
 st.write('hello web app here')
 
 try:
   st.write(requests.get('http://179.50.15.34:5000').text)
 except:
-  print(traceback.format_exc())
+  print(sys.exc_info()[2])
